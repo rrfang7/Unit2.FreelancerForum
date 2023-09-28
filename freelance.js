da5544f04b465fa8970b5a6a4d7f65973a1c39ad
Ralph Fang <ralphrainierfang@yahoo.com>
2023-09-28 19:37:05 -0400

    Add files via upload
    "Added setInterval() & render function"

--- freelance.js
+++ freelance.js
@@ -4,10 +4,27 @@ const occupation = ["Writer", "Teacher", "Programmer", "Driver"]
 const maxFreelancers = 8;
 const freelancers = [
     { 
-        name: "Alice", price: 30, occupation: "writer"
+        name: "Alice", price: 30, occupation: "Writer"
     },
     {
-         name: "Bob", price: 50, occupation: "teacher" 
+         name: "Bob", price: 50, occupation: "Teacher" 
     },
+    {
+        name: "Carol", price: 70, occupation: "Programmer"
+    }
   ];
 
+// setInterval to call addFreelancer every 3 seconds.
+  const addFreelancerIntervalId = setInterval(addFreelancer, 3000);
+
+  render(); 
+
+  function render() { 
+    const nameList = document.querySelector("#names ul");
+
+    const occupationList = document.querySelector("#occupations ul");
+    
+    const startPriceList = document.querySelector("#start-prices ul");
+  }
+
+  
