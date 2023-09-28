d9faa88c980f19673383c7977808e733eca6dd56
Ralph Fang <ralphrainierfang@yahoo.com>
2023-09-28 19:08:25 -0400

    Add files via upload
    "Updated array list"

--- freelance.js
+++ freelance.js
@@ -1,13 +1,13 @@
+// Add ARRAYS name / price / occupation
+const names = ["Alice", "Bob", "Carol", "Rob", "Leia","Mich", "John","Alie"];
+const occupation = ["Writer", "Teacher", "Programmer", "Driver"]
+const maxFreelancers = 8;
 const freelancers = [
-    { name: "Alice", price: 30, occupation: "writer" },
-    { name: "Bob", price: 50, occupation: "teacher" },
-    { name: "Carol", price: 70, occupation: "programmer" },
-    { name: "Rob", price: 81, occupation: "teacher" },
-    { name: "Ned", price: 43, occupation: "teacher" },
-    { name: "John", price: 76, occupation: "programmer" },
-    { name: "Mich", price: 47, occupation: "teacher" },
-    { name: "Leia", price: 72, occupation: "driver" },
+    { 
+        name: "Alice", price: 30, occupation: "writer"
+    },
+    {
+         name: "Bob", price: 50, occupation: "teacher" 
+    },
   ];
 
-
-  
