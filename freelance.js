3ec41e548fa4f064b257d5e98b9fc4750ae3a914
Ralph Fang <ralphrainierfang@yahoo.com>
2023-09-28 21:09:57 -0400

    Add files via upload
    "More render function update"

--- freelance.js
+++ freelance.js
@@ -21,10 +21,40 @@ const freelancers = [
 
   function render() { 
     const nameList = document.querySelector("#names ul");
-
     const occupationList = document.querySelector("#occupations ul");
-    
     const startPriceList = document.querySelector("#start-prices ul");
+
+  freelancers.forEach((freelancer) => {
+    const nameElement = document.createElement("li");
+    const occupationElement = document.createElement("li");
+    const startPriceElement = document.createElement("li");
+    
+    nameElement.textContent = freelancer.name;
+    occupationElement.textContent = freelancer.occupation;
+    startPriceElement.textContent = `$${freelancer.startPrice}`;
+
+    nameList.appendChild(nameElement);
+    occupationList.appendChild(occupationElement);
+    startPriceList.appendChild(startPriceElement);
+
+  });
+  
+    // render average-price
+    const averageStartPrice = calculateAverageStartPrice();
+    document.getElementById("average-price").textContent = `$${freelancer.startPrice}`;
+
   }
 
-  
+  function calculateAverageStartPrice() {
+    let totalStartPrice = 0; 
+
+    for(let i = 0; i < freelancers.length; i++) {
+        totalStartPrice += freelancers[i].startPrice;
+    }
+    const averageStartPrice = totalStartPrice / freelancers.length;
+    return averageStartPrice
+
+  }
+
+
+
